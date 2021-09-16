d3ffa26d1dc747ced7cb7e13c4e29c91e6c3ad71
Blayne Chard <bchard@linz.govt.nz>
2021-09-17 10:57:32 +1200

    wip

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 node_modules/.bin/stac-node-validator \
-    --schemaMap=https://stac-extensions.github.io/template/v1.0.0/schema.json=extensions/template/schema.json \
+    --schemaMap=https://linz.github.io/stac/__STAC_VERSION__/template/schema.json=extensions/template/schema.json \
     --schemaMap=https://linz.github.io/stac/__STAC_VERSION__/camera/schema.json=extensions/camera/schema.json \
     --schemaMap=https://linz.github.io/stac/__STAC_VERSION__/quality/schema.json=extensions/quality/schema.json \
     --schemaMap=https://linz.github.io/stac/__STAC_VERSION__/linz/schema.json=extensions/linz/schema.json \
