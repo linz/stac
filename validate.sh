cb552467f1cbcadfb89898e24975ef025481d005
Blayne Chard <bchard@linz.govt.nz>
2021-09-17 11:54:25 +1200

    fix: allow white space to be passed in

--- validate.sh
+++ validate.sh
@@ -4,4 +4,4 @@ node_modules/.bin/stac-node-validator \
     --schemaMap=https://linz.github.io/stac/__STAC_VERSION__/camera/schema.json=extensions/camera/schema.json \
     --schemaMap=https://linz.github.io/stac/__STAC_VERSION__/quality/schema.json=extensions/quality/schema.json \
     --schemaMap=https://linz.github.io/stac/__STAC_VERSION__/linz/schema.json=extensions/linz/schema.json \
-    $@
+    ${@+"$@"}
