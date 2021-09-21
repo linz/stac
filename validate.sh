c8b889d02d6850260d8ab0665d391db53176144d
Mitchell Paff <mpaff@linz.govt.nz>
2021-09-22 11:00:01 +1200

    feat: added negative json examples to verify schemas negatively

--- validate.sh
+++ validate.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
-node_modules/.bin/stac-node-validator \
-    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/template/schema.json=extensions/template/schema.json \
-    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/camera/schema.json=extensions/camera/schema.json \
-    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/quality/schema.json=extensions/quality/schema.json \
-    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/linz/schema.json=extensions/linz/schema.json \
-    ${@+"$@"}
+
+validator_command=(
+    node_modules/.bin/stac-node-validator
+    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/template/schema.json=extensions/template/schema.json
+    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/camera/schema.json=extensions/camera/schema.json
+    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/quality/schema.json=extensions/quality/schema.json
+    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/linz/schema.json=extensions/linz/schema.json
+)
+"${validator_command[@]}" extensions/*/examples/*.json
+
+for path in extensions/*/non-examples/*.json
+do
+    ! "${validator_command[@]}" "$path"
+done
