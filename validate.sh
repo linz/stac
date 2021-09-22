f5acad2155e76595081629a5f18f05575f413a76
Mitchell Paff <77649372+MitchellPaff@users.noreply.github.com>
2021-09-23 11:12:47 +1200

    feat: added negative json examples to verify schemas negatively (#46)
    * feat: added negative json examples to verify schemas negatively
    
    * fix: add safety pragmas
    
    * fix: remove unnecessary non-eamples
    
    * fix: add non-eamples for other etensions
    
    * fix: fixed the quality schema to actually validate the quality fields

--- validate.sh
+++ validate.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
-node_modules/.bin/stac-node-validator \
-    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/template/schema.json=extensions/template/schema.json \
-    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/camera/schema.json=extensions/camera/schema.json \
-    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/quality/schema.json=extensions/quality/schema.json \
-    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/linz/schema.json=extensions/linz/schema.json \
-    ${@+"$@"}
+
+set -o errexit -o nounset
+shopt -s failglob
+
+validator_command=(
+    node_modules/.bin/stac-node-validator
+    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/template/schema.json=extensions/template/schema.json
+    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/camera/schema.json=extensions/camera/schema.json
+    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/quality/schema.json=extensions/quality/schema.json
+    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/linz/schema.json=extensions/linz/schema.json
+)
+"${validator_command[@]}" extensions/*/examples/*.json
+
+for path in extensions/*/non-examples/*.json
+do
+    if "${validator_command[@]}" "$path"
+    then
+        echo "Valid non-example: ${path}" >&2
+        exit_code=1
+    fi
+done
+
+exit "${exit_code-0}"
