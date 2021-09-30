bc3a6bb6c794186d6e5fd7ba7c2308b071ba5e7b
Blayne Chard <bchard@linz.govt.nz>
2021-09-30 16:56:04 +1300

    fix: correct linting

--- extensions/camera/tests/camera.test.js
+++ extensions/camera/tests/camera.test.js
@@ -1,5 +1,5 @@
-import o from 'ospec'
-import Ajv from 'ajv'
+import o from 'ospec';
+import Ajv from 'ajv';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { promises as fs } from 'fs';
@@ -8,38 +8,37 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const schemaPath = join(__dirname, '..', 'schema.json');
 const examplePath = join(__dirname, '..', 'examples/item.json');
 
-o.spec("camera", ()=>{
-  const ajv = new Ajv()
-  let validate
+o.spec('camera', () => {
+  const ajv = new Ajv();
+  let validate;
 
-  o.before(async ()=> {
-    const data = JSON.parse(await fs.readFile(schemaPath))
-    validate =  ajv.compile(data)
-  })
+  o.before(async () => {
+    const data = JSON.parse(await fs.readFile(schemaPath));
+    validate = ajv.compile(data);
+  });
 
-  o("camera-validates-successfully", async()=>{
+  o('camera-validates-successfully', async () => {
     // given
-    const camera_item_example = JSON.parse(await fs.readFile(examplePath))
+    const camera_item_example = JSON.parse(await fs.readFile(examplePath));
 
     // when
-    let valid = validate(camera_item_example)
+    let valid = validate(camera_item_example);
 
     // then
-    o(valid).equals(true)
-  })
+    o(valid).equals(true);
+  });
 
-  o("camera-validation-fails", async()=>{
+  o('camera-validation-fails', async () => {
     // given
-    const camera_item_example = JSON.parse(await fs.readFile(examplePath))
-    camera_item_example.properties["camera:sequence_number"] = "incorrect_value"
+    const camera_item_example = JSON.parse(await fs.readFile(examplePath));
+    camera_item_example.properties['camera:sequence_number'] = 'incorrect_value';
 
     // when
-    let valid = validate(camera_item_example)
+    let valid = validate(camera_item_example);
 
     // then
-    o(valid).equals(false)
-    o(validate.errors.length).equals(1)
-    o(validate.errors[0].message).equals("should be integer")
-  })
-
-})
+    o(valid).equals(false);
+    o(validate.errors.length).equals(1);
+    o(validate.errors[0].message).equals('should be integer');
+  });
+});
