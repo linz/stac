cc86bea8f3e61ac3a5e8113ba4dbef689075ed4a
Mitchell Paff <mpaff@linz.govt.nz>
2021-10-06 15:22:07 +1300

    fix: cache schemas from online

--- extensions/validation.js
+++ extensions/validation.js
@@ -6,18 +6,30 @@ import iriFormats from 'stac-node-validator/iri.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const Schemas = new Map();
+export function loadSchema(uri) {
+  let existing = Schemas.get(uri);
+  if (existing == null) {
+    existing = loadSchemaFromUri(uri);
+    Schemas.set(uri, existing);
+  }
+  return existing;
+}
+
 /**
  * function passed in to Ajv instance which allows us to load schemas from a url at run time.
  */
-export async function loadSchema(uri) {
+export async function loadSchemaFromUri(uri) {
   try {
     if (uri.startsWith('https://linz.github.io/stac/_STAC_VERSION_/')) {
       const schemaPath = uri.slice('https://linz.github.io/stac/_STAC_VERSION_/'.length);
-
+      console.log('loaded from file', schemaPath);
       return JSON.parse(await fs.readFile(join(__dirname, schemaPath)));
     }
 
-    const response = await axios.get(uri);
+    console.log('Fetch', uri);
+
+    let response = await axios.get(uri);
     return response.data;
   } catch (error) {
     throw new Error(`-- Schema at '${uri}' not found. Please ensure all entries in 'stac_extensions' are valid.`);
