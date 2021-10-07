d1af8ed767b03c50e7ea72d1ea0ded9a961f0e97
Mitchell Paff <77649372+MitchellPaff@users.noreply.github.com>
2021-10-08 11:00:16 +1300

    feat: add unit tests for template extension (#95)
    * feat: add unit tests for template extension
    
    * fix: add readable test names

---
+++ extensions/template/tests/template_item.test.js
@@ -0,0 +1,46 @@
+import o from 'ospec';
+import Ajv from 'ajv';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { promises as fs } from 'fs';
+import { AjvOptions } from '../../validation.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const schemaPath = join(__dirname, '..', 'schema.json');
+const exampleItemPath = join(__dirname, '..', 'examples/item.json');
+
+o.spec('template-item', () => {
+  o.specTimeout(20000);
+  let validate;
+  const ajv = new Ajv(AjvOptions);
+
+  o.before(async () => {
+    const data = JSON.parse(await fs.readFile(schemaPath));
+    validate = await ajv.compileAsync(data);
+  });
+
+  o('Item should pass validation', async () => {
+    // given
+    const templateItemExample = JSON.parse(await fs.readFile(exampleItemPath));
+
+    // when
+    let valid = validate(templateItemExample);
+
+    // then
+    o(valid).equals(true)(JSON.stringify(validate.errors, null, 2));
+  });
+
+  o("Item without mandatory 'template:new_field' property should fail validation", async () => {
+    // given
+    const templateItemExample = JSON.parse(await fs.readFile(exampleItemPath));
+    delete templateItemExample.properties['template:new_field'];
+    // when
+    let valid = validate(templateItemExample);
+
+    // then
+    o(valid).equals(false);
+    o(
+      validate.errors.some((error) => error.message === "should have required property '['template:new_field']'"),
+    ).equals(true)(JSON.stringify(validate.errors));
+  });
+});
