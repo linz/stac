5c8c1fc512b7426ac7258a64277fc1b790489b09
Victor Engmark <vengmark@linz.govt.nz>
2021-10-08 01:44:48 +0000

    refactor: Change LINZ non-examples to unit tests (#97)
    * refactor: Change unsupported 'linz:*' property test to unit test
    
    * refactor: Change unsupported 'quality:*' property test to unit test
    
    * tests: Verify that the error is related to the right property
    
    * refactor: Change invalid `proj:epsg` example to unit test
    
    * refactor: Change missing property non-examples to unit tests

--- extensions/linz/tests/linz_collection.test.js
+++ extensions/linz/tests/linz_collection.test.js
@@ -30,6 +30,93 @@ o.spec('linz-collection', () => {
     o(valid).equals(true)(JSON.stringify(validate.errors, null, 2));
   });
 
+  o("Collection with unsupported 'linz:*' property should fail validation", async () => {
+    // given
+    const collection = JSON.parse(await fs.readFile(examplePath));
+    const parameterName = 'linz:unknown';
+    collection[parameterName] = 1;
+
+    // when
+    let valid = validate(collection);
+
+    // then
+    o(valid).equals(false);
+    o(
+      validate.errors.some(
+        (error) =>
+          error.params.additionalProperty === parameterName &&
+          error.message === 'should NOT have additional properties',
+      ),
+    ).equals(true)(JSON.stringify(validate.errors));
+  });
+
+  o("Collection with unsupported 'quality:*' property should fail validation", async () => {
+    // given
+    const collection = JSON.parse(await fs.readFile(examplePath));
+    const parameterName = 'quality:unknown';
+    collection[parameterName] = 1;
+
+    // when
+    let valid = validate(collection);
+
+    // then
+    o(valid).equals(false);
+    o(
+      validate.errors.some(
+        (error) =>
+          error.params.additionalProperty === parameterName &&
+          error.message === 'should NOT have additional properties',
+      ),
+    ).equals(true)(JSON.stringify(validate.errors));
+  });
+
+  o("Collection with invalid 'proj:epsg' value should fail validation", async () => {
+    // given
+    const collection = JSON.parse(await fs.readFile(examplePath));
+    collection['assets']['example']['proj:epsg'] = 'string';
+
+    // when
+    let valid = validate(collection);
+
+    // then
+    o(valid).equals(false);
+    o(
+      validate.errors.some(
+        (error) => error.dataPath === ".assets['example']['proj:epsg']" && error.message === 'should be integer,null',
+      ),
+    ).equals(true)(JSON.stringify(validate.errors));
+  });
+
+  o("Collection without 'linz:created' property should fail validation", async () => {
+    // given
+    const collection = JSON.parse(await fs.readFile(examplePath));
+    delete collection['linz:created'];
+
+    // when
+    let valid = validate(collection);
+
+    // then
+    o(valid).equals(false);
+    o(validate.errors.some((error) => error.message === "should have required property '['linz:created']'")).equals(
+      true,
+    )(JSON.stringify(validate.errors));
+  });
+
+  o("Collection without 'linz:lifecycle' property should fail validation", async () => {
+    // given
+    const collection = JSON.parse(await fs.readFile(examplePath));
+    delete collection['linz:lifecycle'];
+
+    // when
+    let valid = validate(collection);
+
+    // then
+    o(valid).equals(false);
+    o(validate.errors.some((error) => error.message === "should have required property '['linz:lifecycle']'")).equals(
+      true,
+    )(JSON.stringify(validate.errors));
+  });
+
   o("Collection without 'linz:providers' property should fail validation", async () => {
     // given
     const collection = JSON.parse(await fs.readFile(examplePath));
@@ -41,6 +128,38 @@ o.spec('linz-collection', () => {
     // then
     o(valid).equals(false);
     o(validate.errors.some((error) => error.message === "should have required property '['linz:providers']'")).equals(
+      true,
+    )(JSON.stringify(validate.errors));
+  });
+
+  o("Collection without 'linz:security_classification' property should fail validation", async () => {
+    // given
+    const collection = JSON.parse(await fs.readFile(examplePath));
+    delete collection['linz:security_classification'];
+
+    // when
+    let valid = validate(collection);
+
+    // then
+    o(valid).equals(false);
+    o(
+      validate.errors.some(
+        (error) => error.message === "should have required property '['linz:security_classification']'",
+      ),
+    ).equals(true)(JSON.stringify(validate.errors));
+  });
+
+  o("Collection without 'linz:updated' property should fail validation", async () => {
+    // given
+    const collection = JSON.parse(await fs.readFile(examplePath));
+    delete collection['linz:updated'];
+
+    // when
+    let valid = validate(collection);
+
+    // then
+    o(valid).equals(false);
+    o(validate.errors.some((error) => error.message === "should have required property '['linz:updated']'")).equals(
       true,
     )(JSON.stringify(validate.errors));
   });
@@ -86,9 +205,41 @@ o.spec('linz-collection', () => {
 
       // then
       o(valid).equals(false);
-      o(validate.errors.some((error) => error.message === 'should contain a valid item')).equals(true)(
-        JSON.stringify(validate.errors),
-      );
+      o(
+        validate.errors.some(
+          (error) => error.dataPath === '.providers' && error.message === 'should contain a valid item',
+        ),
+      ).equals(true)(JSON.stringify(validate.errors));
     }
+  });
+
+  o("Collection without 'title' property should fail validation", async () => {
+    // given
+    const collection = JSON.parse(await fs.readFile(examplePath));
+    delete collection.title;
+
+    // when
+    let valid = validate(collection);
+
+    // then
+    o(valid).equals(false);
+    o(validate.errors.some((error) => error.message === "should have required property '.title'")).equals(true)(
+      JSON.stringify(validate.errors),
+    );
+  });
+
+  o("Collection without 'version' property should fail validation", async () => {
+    // given
+    const collection = JSON.parse(await fs.readFile(examplePath));
+    delete collection.version;
+
+    // when
+    let valid = validate(collection);
+
+    // then
+    o(valid).equals(false);
+    o(validate.errors.some((error) => error.message === "should have required property '.version'")).equals(true)(
+      JSON.stringify(validate.errors),
+    );
   });
 });
