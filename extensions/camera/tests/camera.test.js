6e0c8870d685f65dca9a4ed1a8a1f0da8a5bfcc2
Victor Engmark <vengmark@linz.govt.nz>
2021-10-11 14:36:02 +1300

    tests: Set same test timeout everywhere

--- extensions/camera/tests/camera.test.js
+++ extensions/camera/tests/camera.test.js
@@ -3,13 +3,14 @@ import Ajv from 'ajv';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { promises as fs } from 'fs';
-import { AjvOptions } from '../../validation.js';
+import { AjvOptions, defaultTimeout } from '../../validation.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const schemaPath = join(__dirname, '..', 'schema.json');
 const examplePath = join(__dirname, '..', 'examples/item.json');
 
 o.spec('Camera item', () => {
+  o.specTimeout(defaultTimeout);
   let validate;
   const ajv = new Ajv(AjvOptions);
 
--- extensions/template/tests/template_collection.test.js
+++ extensions/template/tests/template_collection.test.js
@@ -3,13 +3,14 @@ import Ajv from 'ajv';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { promises as fs } from 'fs';
-import { AjvOptions } from '../../validation.js';
+import { AjvOptions, defaultTimeout } from '../../validation.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const schemaPath = join(__dirname, '..', 'schema.json');
 const examplePath = join(__dirname, '..', 'examples/collection.json');
 
 o.spec('Template collection', () => {
+  o.specTimeout(defaultTimeout);
   let validate;
   const ajv = new Ajv(AjvOptions);
 
