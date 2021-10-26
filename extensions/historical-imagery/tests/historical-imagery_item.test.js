39a4cef93440c0c8708d32c839f96fca8729bebe
Alice Fage <afage@linz.govt.nz>
2021-10-26 14:57:14 +1300

    added tests

--- extensions/historical-imagery/tests/historical-imagery_item.test.js
+++ extensions/historical-imagery/tests/historical-imagery_item.test.js
@@ -24,23 +24,65 @@ o.spec('historical-imagery item', () => {
     const example = JSON.parse(await fs.readFile(examplePath));
 
     // when
-    let valid = validate(example);
+    const valid = validate(example);
 
     // then
     o(valid).equals(true)(JSON.stringify(validate.errors, null, 2));
   });
 
-  //   o("Example without mandatory 'proj:epsg' property should fail validation", async () => {
-  //     // given
-  //     const example = JSON.parse(await fs.readFile(examplePath));
-  //     delete example.properties['proj:epsg'];
-  //     // when
-  //     let valid = validate(example);
+  o("Example without mandatory 'proj:epsg' property should fail validation", async () => {
+    // given
+    const example = JSON.parse(await fs.readFile(examplePath));
+    delete example.properties['proj:epsg'];
+    // when
+    const valid = validate(example);
 
-  //     // then
-  //     o(valid).equals(false);
-  //     o(validate.errors.some((error) => error.message === "should have required property '['proj:epsg']'")).equals(true)(
-  //       JSON.stringify(validate.errors),
-  //     );
-  //   });
+    // then
+    o(valid).equals(false);
+    o(validate.errors.some((error) => error.message === "should have required property '['proj:epsg']'")).equals(true)(
+      JSON.stringify(validate.errors),
+    );
+  });
+
+  o("Example without mandatory 'mission' property should fail validation", async () => {
+    // given
+    const example = JSON.parse(await fs.readFile(examplePath));
+    delete example.properties['mission'];
+    // when
+    const valid = validate(example);
+
+    // then
+    o(valid).equals(false);
+    o(validate.errors.some((error) => error.message === "should have required property '.mission'")).equals(true)(
+      JSON.stringify(validate.errors),
+    );
+  });
+
+  o("Example without mandatory 'platform' property should fail validation", async () => {
+    // given
+    const example = JSON.parse(await fs.readFile(examplePath));
+    delete example.properties['platform'];
+    // when
+    const valid = validate(example);
+
+    // then
+    o(valid).equals(false);
+    o(validate.errors.some((error) => error.message === "should have required property '.platform'")).equals(true)(
+      JSON.stringify(validate.errors),
+    );
+  });
+
+  o("Example without mandatory 'scan:is_original' property should fail validation", async () => {
+    // given
+    const example = JSON.parse(await fs.readFile(examplePath));
+    delete example.properties['scan:is_original'];
+    // when
+    const valid = validate(example);
+
+    // then
+    o(valid).equals(false);
+    o(validate.errors.some((error) => error.message === "should have required property '['scan:is_original']'")).equals(
+      true,
+    )(JSON.stringify(validate.errors));
+  });
 });
