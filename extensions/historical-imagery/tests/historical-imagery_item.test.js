9d12167e1854d6c73e4642cdafccaee341fb4ecd
Alice Fage <afage@linz.govt.nz>
2021-10-27 14:12:36 +1300

    fix: add eo:bands remove proj:epsg as required fields

--- extensions/historical-imagery/tests/historical-imagery_item.test.js
+++ extensions/historical-imagery/tests/historical-imagery_item.test.js
@@ -30,19 +30,53 @@ o.spec('historical-imagery item', () => {
     o(valid).equals(true)(JSON.stringify(validate.errors, null, 2));
   });
 
-  o("Example without mandatory 'proj:epsg' property should fail validation", async () => {
+  //   o("Example without the mandatory 'aerial-photo:run' field should fail validation", async () => {
+  //     // given
+  //     const example = JSON.parse(await fs.readFile(examplePath));
+  //     delete example.properties['aerial-photo:run'];
+
+  //     // when
+  //     let valid = validate(example);
+
+  //     // then
+  //     o(valid).equals(false);
+  //     o(
+  //       validate.errors.some(
+  //         (error) =>
+  //           error.dataPath === '.properties' && error.message === "should have required property '['aerial-photo:run']'",
+  //       ),
+  //     ).equals(true)(JSON.stringify(validate.errors));
+  //   });
+
+  o("Item with no 'eo:bands' property should fail validation", async () => {
     // given
     const example = JSON.parse(await fs.readFile(examplePath));
-    delete example.properties['proj:epsg'];
+    delete example.properties['eo:bands'];
     // when
-    const valid = validate(example);
+    let valid = validate(example);
 
     // then
     o(valid).equals(false);
-    o(validate.errors.some((error) => error.message === "should have required property '['proj:epsg']'")).equals(true)(
-      JSON.stringify(validate.errors),
-    );
+    o(
+      validate.errors.some(
+        (error) => error.dataPath === '.properties' && error.message === "should have required property '['eo:bands']'",
+      ),
+    ).equals(true)(JSON.stringify(validate.errors));
   });
+
+  //   o("Example without mandatory 'proj:epsg' property should fail validation", async () => {
+  //     // given
+  //     const example = JSON.parse(await fs.readFile(examplePath));
+  //     delete example.properties['proj:epsg'];
+  //     // when
+  //     const valid = validate(example);
+
+  //     // then
+  //     o(valid).equals(false);
+  //     o(validate.errors.some((error) => error.message === "should have required property '['proj:epsg']'")).equals(true)(
+  //       JSON.stringify(validate.errors),
+  //     );
+  //   });
 
   o("Example without mandatory 'mission' property should fail validation", async () => {
     // given
@@ -71,4 +105,41 @@ o.spec('historical-imagery item', () => {
       JSON.stringify(validate.errors),
     );
   });
+
+  //   o("Example without the mandatory 'aerial-photo:run' field should fail validation", async () => {
+  //     // given
+  //     const example = JSON.parse(await fs.readFile(examplePath));
+  //     delete example.properties['aerial-photo:run'];
+
+  //     // when
+  //     let valid = validate(example);
+
+  //     // then
+  //     o(valid).equals(false);
+  //     o(
+  //       validate.errors.some(
+  //         (error) =>
+  //           error.dataPath === '.properties' && error.message === "should have required property '['aerial-photo:run']'",
+  //       ),
+  //     ).equals(true)(JSON.stringify(validate.errors));
+  //   });
+
+  //   o("Example with an incorrect 'aerial-photo:run' field should fail validation", async () => {
+  //     // given
+  //     const example = JSON.parse(await fs.readFile(examplePath));
+  //     example.properties['aerial-photo:run'] = 1234;
+
+  //     // when
+  //     let valid = validate(example);
+
+  //     // then
+  //     o(valid).equals(false);
+
+  //     console.log(o(validate.errors.some(error)));
+  //     o(
+  //       validate.errors.some(
+  //         (error) => error.dataPath === ".properties['aerial-photo:run']" && error.message === 'should be string',
+  //       ),
+  //     ).equals(true)(JSON.stringify(validate.errors));
+  //   });
 });
