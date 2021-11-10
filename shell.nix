614aa4cee12c985f63bf55c69a3ee7231ad2f2fd
Victor Engmark <vengmark@linz.govt.nz>
2021-11-10 14:11:09 +1300

    feat: Enable Jupyter in Nix shell

--- shell.nix
+++ shell.nix
@@ -7,9 +7,26 @@ let
         sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
       })
     { };
+
+  jupyter = import
+    (
+      builtins.fetchGit {
+        url = https://github.com/tweag/jupyterWith;
+        rev = "73bdac9ca036c0303fc3a487129e23f9c4ad0bcf";
+      })
+    { };
+  ipython = jupyter.kernels.iPythonWith {
+    name = "stac";
+    packages = ipythonPackages: with ipythonPackages; [ jsonschema ];
+  };
+  jupyterEnvironment =
+    jupyter.jupyterlabWith {
+      kernels = [ ipython ];
+    };
 in
 pkgs.mkShell {
   buildInputs = [
+    jupyterEnvironment
     pkgs.nodejs
     pkgs.yarn
   ];
