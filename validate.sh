fff84dc70fd847c8e8fb138d90f07b9b5542b838
Alice Fage <afage@linz.govt.nz>
2021-11-12 11:54:04 +1300

    fix: change linz.github.io/stac to stac.linz.govt.nz (#149)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- validate.sh
+++ validate.sh
@@ -5,14 +5,14 @@ shopt -s failglob globstar
 
 validator_command=(
     node_modules/.bin/stac-node-validator
-    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/template/schema.json=extensions/template/schema.json
-    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/historical-imagery/schema.json=extensions/historical-imagery/schema.json
-    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/aerial-photo/schema.json=extensions/aerial-photo/schema.json
-    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/camera/schema.json=extensions/camera/schema.json
-    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/film/schema.json=extensions/film/schema.json
-    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/scanning/schema.json=extensions/scanning/schema.json
-    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/quality/schema.json=extensions/quality/schema.json
-    --schemaMap=https://linz.github.io/stac/_STAC_VERSION_/linz/schema.json=extensions/linz/schema.json
+    --schemaMap=https://stac.linz.govt.nz/_STAC_VERSION_/template/schema.json=extensions/template/schema.json
+    --schemaMap=https://stac.linz.govt.nz/_STAC_VERSION_/historical-imagery/schema.json=extensions/historical-imagery/schema.json
+    --schemaMap=https://stac.linz.govt.nz/_STAC_VERSION_/aerial-photo/schema.json=extensions/aerial-photo/schema.json
+    --schemaMap=https://stac.linz.govt.nz/_STAC_VERSION_/camera/schema.json=extensions/camera/schema.json
+    --schemaMap=https://stac.linz.govt.nz/_STAC_VERSION_/film/schema.json=extensions/film/schema.json
+    --schemaMap=https://stac.linz.govt.nz/_STAC_VERSION_/scanning/schema.json=extensions/scanning/schema.json
+    --schemaMap=https://stac.linz.govt.nz/_STAC_VERSION_/quality/schema.json=extensions/quality/schema.json
+    --schemaMap=https://stac.linz.govt.nz/_STAC_VERSION_/linz/schema.json=extensions/linz/schema.json
 )
 
 "${validator_command[@]}" ./**/examples/*.json
